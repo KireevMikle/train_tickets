c431743bc0ed267c7c5142a4c94e3ce52dce57c7
Kireev Mikle <kireev.mikle@gmail.com>
2025-08-24 12:23:59 +0100

    update2

--- train_tikets_app/models.py
+++ train_tikets_app/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Type(models.Model):
+    type = models.CharField(max_length=100)
+    description = models.TextField()
+
+
+class Train(models.Model):
+    name = models.CharField(max_length=100)
+    type = models.ForeignKey(Type, on_delete=models.CASCADE)
+
+class Seat(models.Model):
+    number = models.IntegerField()
+    train = models.ForeignKey(Train, on_delete=models.CASCADE)
+
+
+class Booking(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateField()
+    seat = models.ForeignKey(Seat, on_delete=models.CASCADE)
+
